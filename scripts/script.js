a63a32640071ce4b8dd368dbb8d2b9177a7e39c1
Natan Brito <natanismaelbrito@gmail.com>
2022-02-18 20:00:10 -0300

    gerando cartas

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,17 @@ function criarQuizz(acionado){
 const telaInicial=document.querySelector(".home")
 telaInicial.classList.add("hidden")
 }
+
+gerandoTodosQuizzes ();
+function gerandoTodosQuizzes (){
+    for(let i=0;i<10;i++){
+        const chamandoTodosQuizzes= document.querySelector(".listaQuizz")
+     chamandoTodosQuizzesHTML= `
+     <div class="quizz">
+     <p class="escritaBrancaQuizz">Acerte os personagens corretos dos Simpsons e prove seu amor! </p>
+     <img src="./images/simpsons.png">
+     </div>
+     `
+     chamandoTodosQuizzes.innerHTML  += chamandoTodosQuizzesHTML;
+}
+}
