a713c34206b789b395318afd725251b17ac94d26
Natan Brito <natanismaelbrito@gmail.com>
2022-02-18 20:32:33 -0300

    commitando pra pegar o pull

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,15 @@ telaInicial.classList.add("hidden")
 
 gerandoTodosQuizzes ();
 function gerandoTodosQuizzes (){
+    let chamandoTodosQuizzes= document.querySelector(".listaQuizz")
     for(let i=0;i<10;i++){
-        const chamandoTodosQuizzes= document.querySelector(".listaQuizz")
-     chamandoTodosQuizzesHTML= `
+     let chamandoTodosQuizzesHTML= `
      <div class="quizz">
      <p class="escritaBrancaQuizz">Acerte os personagens corretos dos Simpsons e prove seu amor! </p>
      <img src="./images/simpsons.png">
      </div>
      `
-     chamandoTodosQuizzes.innerHTML  += chamandoTodosQuizzesHTML;
-}
+     chamandoTodosQuizzes.innerHTML += chamandoTodosQuizzesHTML;
+     console.log("25")
+    }
 }
