60ce190124d7740df0a7602115646d3918b4c8f4
João Victor Francisco <Joo Victor>
2022-02-18 21:11:56 -0300

    testes js, mudança do nome da classe hidden

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,20 @@
+let chamandoTodosQuizzes = document.querySelector(".listaQuizz")
+
 function criarQuizz(acionado){
 const telaInicial=document.querySelector(".home")
 telaInicial.classList.add("hidden")
 }
 
-gerandoTodosQuizzes ();
+
 function gerandoTodosQuizzes (){
+    alert('alou')
     for(let i=0;i<10;i++){
-        const chamandoTodosQuizzes= document.querySelector(".listaQuizz")
-     chamandoTodosQuizzesHTML= `
-     <div class="quizz">
-     <p class="escritaBrancaQuizz">Acerte os personagens corretos dos Simpsons e prove seu amor! </p>
-     <img src="./images/simpsons.png">
-     </div>
-     `
-     chamandoTodosQuizzes.innerHTML  += chamandoTodosQuizzesHTML;
+        chamandoTodosQuizzes.innerHTML += `
+        <div class="quizz">
+            <p class="escritaBrancaQuizz"> Acerte os personagens corretos dos Simpsons e prove seu amor! </p>
+            <img src="./images/simpsons.png">
+        </div>
+    `
+    }
 }
-}
+
