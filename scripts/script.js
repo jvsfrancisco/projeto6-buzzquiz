60ddf5777aef7cf6cefec1112896edc3b116e240
João Victor Francisco <Joo Victor>
2022-02-19 01:59:07 -0300

    impressão automática quizzes homepage

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,22 @@
 let allquizz = document.querySelector(".listaQuizz")
+const telaInicial = document.querySelector(".home")
+let quizzPage = document.querySelector(".quizz-page")
+
 
 function criarQuizz(acionado) {
-    const telaInicial = document.querySelector(".home")
-    telaInicial.classList.add("hidden")
+    telaInicial.classList.add("disappear")
 }
 
-function quizzRender() {
+homePageRender()
+
+function homePageRender() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     promise.then(quizGenerator);
 }
 
 function quizGenerator(quizz) {
     let arrayquizz = quizz.data
-    for (let i = 0; i < arrayquizz.length; i++) {
-        quizzGeral(arrayquizz[i]);
-    }
+    arrayquizz.forEach(quizzGeral);
 }
 
 // function QuizzUsuario(message){
@@ -33,9 +35,50 @@ function quizGenerator(quizz) {
 
 function quizzGeral(quizz) {
     allquizz.innerHTML += `
-    <div class="quizz">
+    <div class="quizz" onclick="openQuizz(${quizz.id})">
         <p class="escritaBrancaQuizz"> ${quizz.title} </p>
         <img src="${quizz.image}" alt = "">
     </div>
 `
 }
+
+function openQuizz(id) {
+    telaInicial.classList.add("disappear")
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
+    promise.then(openQuizzRender)
+}
+
+function openQuizzRender(quizz) {
+    // quizzPage.classList.remove("disappear")
+    console.log(quizz.data)
+    console.log(quizzPage)
+
+    let arrayQuizz = quizz.data
+
+    quizzPage.innerHTML += `<nav class="sub-header">
+            <p>${arrayQuizz.title}</p>
+        </nav>`
+    let arrayQuestions = arrayQuizz.questions
+
+    arrayQuestions.forEach(function () {
+        quizzPage.innerHTML += `<nav class="questions">
+            <article>
+                <div class="question-title">
+                    <p>${arrayQuestions.title}</p>
+                </div>`
+
+        let arrayAnswers = arrayQuizz.questions.answers
+
+        arrayAnswers.forEach(function (){
+            quizzPage.innerHTML += `<div class="answer">
+                        <div class="alternatives ${arrayAnswers.isCorrectAnswer}">
+                            <img src="${arrayAnswers.image}" alt="simpson">
+                            <p>${arrayAnswers.text}</p>
+                        </div>`
+        })
+        quizzPage.innerHTML += `</article>
+                            </nav>`
+    })
+}
+
+
