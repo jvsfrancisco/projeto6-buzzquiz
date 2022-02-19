ab5ead4f74a5c8691acf6ca5f9f7a812cf96561f
João Victor Francisco <Joo Victor>
2022-02-19 20:01:15 -0300

    Quizzpage

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let allquizz = document.querySelector(".listaQuizz")
 const telaInicial = document.querySelector(".home")
-let quizzPage = document.getElementsByClassName("quizz-page")
+const quizzPage = document.querySelector(".quizz-page")
 
 
 function criarQuizz(acionado) {
@@ -10,7 +10,7 @@ function criarQuizz(acionado) {
 homePageRender()
 
 function homePageRender() {
-    const promise = axios.get("https:mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     promise.then(quizGenerator);
 }
 
@@ -43,43 +43,38 @@ function quizzGeral(quizz) {
 }
 
 function openQuizz(id) {
+    console.log(telaInicial)
     telaInicial.classList.add("disappear")
-    const promise = axios.get(`//https:mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
     promise.then(openQuizzRender)
 }
 
 function openQuizzRender(quizz) {
     quizzPage.classList.remove("disappear")
-    console.log(quizz.data)
-    console.log(quizzPage)
-
     let arrayQuizz = quizz.data
-
     quizzPage.innerHTML += `<nav class="sub-header">
             <p>${arrayQuizz.title}</p>
         </nav>`
     let arrayQuestions = arrayQuizz.questions
 
-    arrayQuestions.foreach(trazer)
-    quizzPage.innerHTML += `</article>
-                            </nav>`
-}
-
-function trazer(arrayQuestions) {
-    quizzPage.innerHTML += `<nav class="questions">
-        <article>
-            <div class="question-title">
-                <p>${arrayQuestions.title}</p>
-            </div>`
-
-    let arrayAnswers = arrayQuizz.questions.answers
-    arrayAnswers.foreach(blabla)
-}
-
-function blabla() {
-    quizzPage.innerHTML += `<div class="answer">
-                <div class="alternatives ${arrayAnswers.isCorrectAnswer}">
-                    <img src="${arrayAnswers.image}" alt="simpson">
-                    <p>${arrayAnswers.text}</p>
+    arrayQuestions.forEach(function (arrayQuestions) {
+        quizzPage.innerHTML += `<nav class="questions">
+            <article>
+                <div class="question-title">
+                    <p>${arrayQuestions.title}</p>
                 </div>`
+
+        let arrayAnswers = arrayQuestions.answers
+
+        arrayAnswers.forEach(function (arrayAnswers){
+            quizzPage.innerHTML += `<div class="answer">
+                        <div class="alternatives ${arrayAnswers.isCorrectAnswer}">
+                            <img src="${arrayAnswers.image}" alt="simpson">
+                            <p>${arrayAnswers.text}</p>
+                        </div>`
+        })
+    })
+    console.log(quizzPage)
 }
+
+
